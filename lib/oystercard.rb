8ab3d1c83f31e145bd26c5c28fbe06309e6cde00
herecomesjaycee <jaycee.cheong04@gmail.com>
2017-01-09 17:00:43 +0000

    Closing #6: Add limit to oystercard

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,16 @@
 class Oystercard
 
-
-attr_reader :balance
+DEFAULT_LIMIT = 90
+attr_reader :balance#, :limit
 
 	def initialize
 	@balance = 0
+	#@limit = DEFAULT_LIMIT
 	end
 
 	def top_up(amount)
+		err_message = "Limit is #{DEFAULT_LIMIT}, declined"
+		raise (err_message) if @balance + amount > DEFAULT_LIMIT
 		@balance += amount 
 	end
 
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -10,17 +10,23 @@ describe Oystercard do
 		#expect(Oystercard.new(20)).to have_attributes(:amount =>20)
 	#end/
 
-
-
 	describe '#top_up' do
 		context 'should allow card to be topped up' do
 			it 'should responds to one arguement' do
-			oystercard = Oystercard.new
-			expect(oystercard).to respond_to(:top_up).with(1).argument
+			expect(subject).to respond_to(:top_up).with(1).argument
 		end
 			it 'should return 12' do
 			expect(subject.top_up(12)).to eq 12
 		end
+
+		context 'Limit should set at 90' do
+			it 'should raise error if limit exceed 90' do
+			stub_const("DEFAULT_LIMIT", 90)
+			err_message = "Limit is #{DEFAULT_LIMIT}, declined"	
+			expect{subject.top_up(91)}.to raise_error(err_message)
+			end
+		end
+
 	end
 
 	end
