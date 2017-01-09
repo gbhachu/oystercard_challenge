2bc54fcdaa4021b9b0f97a6161b4a8e044a56d62
herecomesjaycee <jaycee.cheong04@gmail.com>
2017-01-09 15:27:16 +0000

    Closing #4: Add initiliaze method

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,3 +1,16 @@
+require 'oystercard'
+
 describe Oystercard do
+
+	it 'should have a balance of 0' do
+		expect(subject).to have_attributes(:balance =>0)
+	end
+
+	/it 'should have a balance of 20' do
+		expect(Oystercard.new(20)).to have_attributes(:amount =>20)
+	end/
+
 end
+
+
 
