e48c4b01394a3ac7a68d7cfc6a9f54125407fcfd
herecomesjaycee <jaycee.cheong04@gmail.com>
2017-01-10 16:25:11 +0000

    Closing #11 - Saving the entry station

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,11 +2,12 @@ class Oystercard
 
 DEFAULT_LIMIT = 90
 MIN_FARE = 1
-attr_reader :balance, :in_journey
+attr_reader :balance, :in_journey, :entry_station
 
 	def initialize
 	@balance = 0
-	@in_journey = false
+	@in_journey = in_journey?
+	@entry_station = nil
 	end
 
 	def top_up(amount)
@@ -14,21 +15,26 @@ attr_reader :balance, :in_journey
 		@balance += amount
 	end
 
-	def touch_in(station)
+	def touch_in(entry_station)
 		raise 'insufficient funds' if @balance < MIN_FARE
 		@in_journey = true
-
+		@entry_station = entry_station
 	end
 
 	def touch_out(station)
 		deduct
 		@in_journey = false
+		@entry_station = nil
 	end
 
 private
 
-def deduct
-	@balance -= MIN_FARE
-end
+	def deduct
+		@balance -= MIN_FARE
+	end
+
+	def in_journey?
+		entry_station == nil ? true : false
+	end
 
 end
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -2,56 +2,68 @@ require 'oystercard'
 
 describe Oystercard do
 subject(:card){ described_class.new}
+let(:entry_station) {double :station}
+let(:exit_station) {double :station}
 
 	describe 'New instance' do
 		it 'should have a balance of 0' do
-		expect(card).to have_attributes(:balance => 0)
+			expect(card).to have_attributes(:balance => 0)
 		end
 	end
 
 	describe '#top_up' do
 		context 'should allow card to be topped up' do
 				it 'should responds to one arguement' do
-				expect(card).to respond_to(:top_up).with(1).argument
+					expect(card).to respond_to(:top_up).with(1).argument
 				end
 				it 'should return 12' do
-				expect(card.top_up(12)).to eq 12
+					expect(card.top_up(12)).to eq 12
 				end
 		end
 
 		context 'Limit should set at 90' do
 			it 'should raise error if limit exceed 90' do
-			default_limit = Oystercard::DEFAULT_LIMIT
-			card.top_up(default_limit)
-			expect{card.top_up(1)}.to raise_error("Limit is #{default_limit}, declined")
+				default_limit = Oystercard::DEFAULT_LIMIT
+				card.top_up(default_limit)
+				expect{card.top_up(1)}.to raise_error("Limit is #{default_limit}, declined")
 			end
 		end
 	end
 
-
 	describe '#touch_in' do
+	#potentially use (:before) do end block here for card.top_up and card.touch_in
 		it 'touches in' do
-		card.top_up(2)
-		card.touch_in("Bank")
-		expect(card).to have_attributes(:in_journey => true)
+			card.top_up(2)
+			card.touch_in(entry_station)
+			expect(card).to have_attributes(:in_journey => true)
 		end
 		it 'raise an error when insufficient funds on card' do
-			expect{card.touch_in('a')}.to raise_error('insufficient funds')
+			expect{card.touch_in(entry_station)}.to raise_error('insufficient funds')
+		end
+		it 'remembers entry station' do
+			card.top_up(2)
+			expect(card.touch_in(entry_station)).to eq entry_station
 		end
 	end
 
 	describe '#touch_out' do
 		it 'touches out' do
-		card.top_up(2)
-		card.touch_in("Bank")
-		card.touch_out("Liverpool Street")
-		expect(card).to have_attributes(:in_journey => false)
+			card.top_up(2)
+			card.touch_in(entry_station)
+			card.touch_out("Liverpool Street")
+			expect(card).to have_attributes(:in_journey => false)
 		end
 		it 'touches out deducts amount' do
 			card.top_up(2)
-			card.touch_in("Bank")
+			card.touch_in(entry_station)
 			expect {card.touch_out("Liverpool Street")}.to change{card.balance}.by(-1)
 
+		end
+		it 'touches out will set entry station to nil' do
+			card.top_up(2)
+			card.touch_in(entry_station)
+			card.touch_out(exit_station)
+			expect(card).to have_attributes(:entry_station => nil)
 		end
 	end
 
